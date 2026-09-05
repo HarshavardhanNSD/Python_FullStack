e3a1f8a8de51ef57b4266c9e75dc773b1cc784f7
Harshavardhan Nibhanupudi <harshavardhannibhanupudi7@gmail.com>
2026-09-05 15:21:16 +0530

    Day 20

---
+++ Day20.py
@@ -0,0 +1,14 @@
+import calculation
+print("Addition:", calculation.add(20,10))
+print("subtraction:",calculation.sub(20,10))
+print("Multiplication:", calculation.mul(20,10))
+print("Division:", calculation.div(20,10))
+
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
